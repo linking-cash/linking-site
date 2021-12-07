452b75bd0ce7d1c80a3317cbb16b1d98a5099e44
linking-project <linkingcryptocurrency@gmail.com>
2021-12-07 15:04:37 -0300

    add javascript

---
+++ js/alert.js
@@ -0,0 +1,10 @@
+let bitcoinAddress = "bc1q6zty63anm3yaw2cytha73ve662jmflylstdag7";
+let bitcoincashAddress = "qpht9rem3hpez0mrshzz9jejwlfrnhac8vluvulwlr";
+let nanoAddress = "nano_1hfonybqu3h9i1xomd33j99oekdwj4ouxqcdphqph16u8ex8iuxyi7qju44z";
+
+function donateAdress() {
+    alert(`BTC - ${bitcoinAddress}`);
+    alert(`BCH - ${bitcoincashAddress}`);
+    alert(`XNO - ${nanoAddress}`);
+    alert(`Thanks for the donation!`);
+} 
